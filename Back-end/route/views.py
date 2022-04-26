70277e75925afb3d71356dae5a621920719648de
Caleb_ <95022986+Mzazi25@users.noreply.github.com>
2022-04-26 21:18:54 +0300

    Added MerchandiserList API

--- Back-end/route/views.py
+++ Back-end/route/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import  Merchandiser,Manager,Route,Comment
+from .serializer import MerchandiserSerializer,ManagerSerializer,RouteSerializer
+from rest_framework import status
+from .permissions import IsAdminOrReadOnly
 
 # Create your views here.
+
+class MerchandiserList(APIView):
+    def get(self, request, format=None):
+        merch = Merchandiser.objects.all()
+        serializers = MerchandiserSerializer(merch, many=True)
+        return Response(serializers.data)
+    def post(self, request, format=None):
+        serializers = MerchandiserSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+    permission_classes = (IsAdminOrReadOnly,)
+
