41f9eaa5f54b2ee2a02aafae58ab6c5d5403e9c8
Caleb_ <95022986+Mzazi25@users.noreply.github.com>
2022-04-26 21:11:19 +0300

    Added Manager Serializers

--- Back-end/route/serializer.py
+++ Back-end/route/serializer.py
@@ -6,3 +6,9 @@ class MerchandiserSerializer(serializers.ModelSerializer):
         model = Merchandiser
         fields = ('username', 'route', 'phone_number','email', 'location')
 
+class ManagerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Manager
+        fields = ('name', 'description', 'route_plan','phone_number', 'location')
+
+
