f6242c40d8f9a5dd57289492c8a7ae4a343f66fc
Caleb_ <95022986+Mzazi25@users.noreply.github.com>
2022-04-26 20:28:43 +0300

    Added Postgres DB

--- Back-end/merchandisers/settings.py
+++ Back-end/merchandisers/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'merchandisers.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'nightwatch',
+        'USER': 'user',
+        'PASSWORD':'Mzazi25',
     }
 }
 
