f5d5fc4d25a218ec50c7de826016f5959e32b648
Caleb_ <95022986+Mzazi25@users.noreply.github.com>
2022-04-26 21:38:29 +0300

    added rest API to the application

--- Back-end/merchandisers/settings.py
+++ Back-end/merchandisers/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'route',
+    'rest_framework.authtoken',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -124,3 +125,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    )
+}
--- Back-end/merchandisers/urls.py
+++ Back-end/merchandisers/urls.py
@@ -22,5 +22,6 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('route.urls')),
+    path('api-token-auth/', obtain_auth_token),
 
 ]
