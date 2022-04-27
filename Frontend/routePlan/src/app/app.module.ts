233784909e2406bd59fc0607e7cedff6921f790a
EsauKip <esau.kiprono@student.moringaschool.com>
2022-04-27 17:35:39 +0300

    add buttons

--- Frontend/routePlan/src/app/app.module.ts
+++ Frontend/routePlan/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { GoogleMapsModule } from '@angular/google-maps';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
-import { CommonModule } from '@angular/common';
 import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
 @NgModule({
   declarations: [
@@ -15,10 +15,9 @@ import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     GoogleMapsModule,
-    CommonModule,
     HttpClientModule,
-    HttpClientJsonpModule
-    
+    HttpClientJsonpModule,
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
